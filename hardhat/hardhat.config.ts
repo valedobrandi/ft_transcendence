00b3e41352b85edc839d79235e63fa1d69bc0b8e
valedobrandi <valedobrandi@gmail.com>
2025-10-21 14:40:18 +0200

    test passed

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,47 +1,31 @@
-import "@nomicfoundation/hardhat-ethers";
 import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  //plugins: [hardhatToolboxMochaEthersPlugin],
-  solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
+    plugins: [hardhatToolboxMochaEthersPlugin],
+    solidity: {
         version: "0.8.28",
         settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
         },
-      },
     },
-  },
-  networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
+    networks: {
+        localhost: {
+            type: "http",
+            chainType: "l1",
+            url: "http://127.0.0.1:8545",
+            chainId: 43112,
+        },
+        hardhatOp: {
+            type: "edr-simulated",
+            chainType: "op",
+        },
     },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
-    sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-    },
-	localhost: {
-		type: "http",
-		chainType: "l1",
-		url: "http://127.0.0.1:8545",
-		chainId: 43112,
-	}
-  },
 };
 
 export default config;
--- hardhat/test/Score.ts
+++ hardhat/test/Score.ts
@@ -1,26 +1,28 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { network } from "hardhat";
+
+let ethers: typeof import("hardhat").ethers;
 
 describe("TournamentScores contract", function () {
+  before(async function () {
+    const result = await network.connect({
+      network: "hardhatOp",
+      chainType: "op",
+    });
+    ethers = result.ethers;
+  });
+
   it("should allow a player to submit and retrieve a score", async function () {
-    // 1. Deploy the contract
     const TournamentScores = await ethers.getContractFactory("TournamentScores");
     const contract = await TournamentScores.deploy();
     await contract.waitForDeployment();
 
-    // 2. Get a test signer (pre-funded account)
     const [player] = await ethers.getSigners();
-
-    // 3. Submit a score
     const tx = await contract.connect(player).submitScore(200);
     await tx.wait();
 
-    // 4. Retrieve the latest score
     const [points, timestamp] = await contract.getLatestScore(player.address);
-
     expect(points).to.equal(200);
     expect(timestamp).to.be.gt(0);
-
-    console.log("Score submitted and retrieved successfully!");
   });
 });
