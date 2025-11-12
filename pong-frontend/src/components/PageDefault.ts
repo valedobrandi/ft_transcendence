bdd5850ae0f984dedb9b5dbeff7c398f73f96c8a
flmarsou <marsouinflorent12@gmail.com>
2025-11-12 16:06:53 +0100

    Modified buttons and added background image

--- pong-frontend/src/components/PageDefault.ts
+++ pong-frontend/src/components/PageDefault.ts
@@ -1,25 +1,27 @@
 import { navigateTo } from "../utils";
-import { Button } from "./Button";
+import { FancyButton } from "./Button";
 
 export function Default(): HTMLElement {
 	const viewDiv = document.createElement("div");
     viewDiv.id = "view-container";
 	viewDiv.className = "flex flex-col items-center h-screen";
+	viewDiv.style.backgroundImage = "url('../../default/default_background.jpg')";
+	viewDiv.style.backgroundSize = "cover";
 
 	const mainDiv = document.createElement("div");
     mainDiv.id = "login-container";
-    mainDiv.className = "flex flex-col gap-2 justify-center items-center mt-[100px]";
+    mainDiv.className = "flex flex-col gap-20 justify-center items-center mt-[200px]";
 
     const title = document.createElement("h1");
-    title.className = "game-font text-6xl text-center mb-10 text-yellow-400 text-shadow-lg/30 pt-20";
+    title.className = "game-font text-6xl text-center mb-10 text-[hsl(345,100%,47%)] text-shadow-lg/30 pt-20";
     title.textContent = "Ft_transcendence Ping-Pong";
     viewDiv.appendChild(title);
 
-	const guestBtn = Button("guest", "h-14 w-60 game-font tracking-widest text-lg", () => navigateTo("/guest"));
+	const guestBtn = FancyButton("guest", "scale-150 h-14 w-60 game-font tracking-widest text-lg", () => navigateTo("/guest"));
 
-    const loginBtn = Button("login", "h-14 w-60 game-font tracking-widest text-lg", () => navigateTo("/login"));
+    const loginBtn = FancyButton("login", "scale-150 h-14 w-60 game-font tracking-widest text-lg", () => navigateTo("/login"));
 
-	const registerBtn = Button("register", "h-14 w-60 game-font tracking-widest text-lg", () => navigateTo("/register"));
+	const registerBtn = FancyButton("register", "scale-150 h-14 w-60 game-font tracking-widest text-lg", () => navigateTo("/register"));
 
 	mainDiv.appendChild(guestBtn);
 	mainDiv.appendChild(loginBtn);
