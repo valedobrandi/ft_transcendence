e1b9febb9515a595a23da56137743001c2ab84c6
Bernardo Ramos <142626511+valedobrandi@users.noreply.github.com>
2025-12-06 09:24:38 +0100

    blockchain not working (#85)
    Co-authored-by: bernardo <bde-albu@42nice.com>

--- pong-frontend/src/endPoints.ts
+++ pong-frontend/src/endPoints.ts
@@ -3,7 +3,7 @@ const VITE_PONG_BACKEND_API = import.meta.env.VITE_PONG_BACKEND_API;
 
 const endpoint = {
 	pong_backend_api: VITE_PONG_BACKEND_API || 'http://localhost:3000/api',
-    	pong_backend_websocket: VITE_PONG_BACKEND_WEBSOCKET || 'ws://localhost:3000/ws',
+    pong_backend_websocket: VITE_PONG_BACKEND_WEBSOCKET || 'ws://localhost:3000/ws',
 }
 
 export { endpoint };
